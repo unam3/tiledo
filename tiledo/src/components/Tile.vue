13c79bbc04fc51d430808f7469f375a26d474562
Flowers Safety <40meta@gmail.com>
2022-09-07 08:49:40 +0600

    slightly modified default vue project was added

---
+++ tiledo/src/components/Tile.vue
@@ -0,0 +1,37 @@
+<script setup>
+//defineProps({
+//  text: {
+//    type: String,
+//    required: true
+//  }
+//})
+</script>
+
+<template>
+  <div class="tile">
+        <input type="text" v-bind:value="Math.PI" />
+        <button>{{ Math.PI }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+.tile {
+  min-height: 10em;
+  min-width: 16em;
+  border: 2px solid #a94646;
+  border-radius: 0.5em;
+  text-align: center;
+}
+
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
+
